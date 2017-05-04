7283f151e5a1c6040c735b74d5c69e30eeb0ffee
TheVaffel <hkon20@hotmail.com>
2017-05-04 16:19:52 +0200

    Added setTitle in test and corrected mistake in compile.sh

--- examples/compile.sh
+++ examples/compile.sh
@@ -5,7 +5,7 @@ HConLibDir="$currDir/.."
 binDir="$currDir/bin"
 g++ "$currDir/cam.cpp" -o "$binDir/cam" -I $HConLibDir -lX11
 g++ "$currDir/test.cpp" -o "$binDir/test" -I $HConLibDir -lX11
-g++ "$currDir/hgraftest.cpp" -o "$binDir/test" -I $HConLibDir -lX11
+g++ "$currDir/hgraftest.cpp" -o "$binDir/hgraftest" -I $HConLibDir -lX11
 g++ "$currDir/linetest.cpp" -o "$binDir/linetest" -I $HConLibDir -lX11
 g++ "$currDir/algtest.cpp" -o "$binDir/algtest" -I $HConLibDir -lX11
 g++ "$currDir/cartoonifycam.cpp" -o "$binDir/cartoonifycam" -I $HConLibDir -I $currDir -lX11
--- examples/test.cpp
+++ examples/test.cpp
@@ -1,5 +1,7 @@
 #define WINVAL_IMPLEMENTATION
 #include "Winval/Winval.h"
+#define HGRAF_IMPLEMENTATION
+#include "HGraf/HGraf.h"
 #include <unistd.h>
 #include <iostream>
 
@@ -20,10 +22,27 @@ int main(){
 
   
   Winval win(w, h, &dummy);
+  win.setTitle("This is a test application");
   win.drawBuffer(p, w, h);
   int down = 0;
   int x, y;
-  while(1){
+	int bound[2][2] = {{0, 0}, {959, 539}};
+	int point[2] = {958, 539};
+	float vec[2] = {-0.972254, 0.233926};
+	int res[2][2];
+
+	hg::getBoundaryIntersections(point, vec, bound, res);
+	cout<<"Got res "<<res[0][0]<<", "<<res[0][1]<<" and "<<res[1][0]<<", "<<res[1][1]<<endl;
+ 
+ 	hg::LineIterator li(959, 539, 0, 539);
+	
+	/*while(!li.isFinished()){
+		cout<<li[0]<<", "<<li[1]<<endl;
+		li++;
+	}*/
+
+	// cout<<li.currPoint[0]<<", "<<li.currPoint[1]<<" and "<<li.endPoint[0]<<", "<<li.endPoint[1]<<endl;
+	while(1){
     for(int i = 0; i < h*w*4; i++){
       p[i] = ~p[i];
     }

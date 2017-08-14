4f002bb567a0e5f9d377b69f3b70cadcde77b314
TheVaffel <hkon20@hotmail.com>
2017-08-14 16:09:46 +0200

    Added initialization file for GLSL compiler

---
+++ initialize_vulkan_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+#get glslang
+git clone git@github.com:KhronosGroup/glslang.git
+
+#build glslang archives
+cd glslang 
+mkdir build
+cd build
+cmake ..
+make -j $(grep -c ^processor /proc/cpuinfo)
+
+#copy libraries
+cp $(find . | grep '\.a$') ../../../lib
+
+#copy headers
+cd ..
+cp -r glslang/ SPIRV/ ../../include/external/vulkan
+
+cd ../..

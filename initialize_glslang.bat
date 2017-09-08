cb9583f887889bab8af14c075839b010a39de0ef
The Vaffel <hkon20@hotmail.com>
2017-09-08 20:10:23 +0200

    Added script for initializing glslang on Windows

---
+++ initialize_glslang.bat
@@ -0,0 +1,22 @@
+mkdir build
+cd build
+
+set conf=Release
+
+git clone https://github.com/KhronosGroup/glslang.git
+
+cd glslang
+mkdir build
+cd build
+cmake ..
+msbuild ALL_BUILD.vcxproj /P:Configuration=%conf%
+
+mkdir ..\..\..\lib
+for /f "usebackq delims=" %%a in (`dir /s /b *.lib`) do copy "%%a" ..\..\..\lib\%conf% /Y
+
+#copy headers
+cd ..
+xcopy glslang /h /e ..\..\include\external\vulkan\glslang /i
+xcopy SPIRV /h /e ..\..\include\external\vulkan\SPIRV /i
+
+cd ..\..
